f4965265926ff14fecfcf3e4e9410466ccdc747d
Gabrielle Ribeiro <33001620+Gabrielle-Ribeiro@users.noreply.github.com>
2023-12-14 16:16:53 -0300

    Update userService.spec.js

--- src/test/userService.spec.js
+++ src/test/userService.spec.js
@@ -5,7 +5,7 @@ const { testeDB } = require('../config/database');
 describe('Testando função emailExiste de userService', () => {
     it('deve retornar FALSE para email invalido', () => {
         const email = 'invalido@mail.com';
-        expect(emailExiste(email, testeDB)).toBe(true);
+        expect(emailExiste(email, testeDB)).toBe(false);
     });
     it('deve retornar FALSE para email inexistente', () => {
         const email = undefined;
